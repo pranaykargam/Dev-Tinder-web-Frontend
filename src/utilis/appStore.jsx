e2aae599af5a9b33a85d559c815d45e3c46d5259
pranaykargam <sunnykargam@gmail.com>
2025-08-16 19:56:36 +0530

    updated

--- src/utilis/appStore.jsx
+++ src/utilis/appStore.jsx
@@ -2,7 +2,7 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
-import connectionsReducer from "./ConnectionsSlice";
+import connectionsReducer from "./connectionsSlice";
 import requestsReducer from "./requestSlice";
 
 
