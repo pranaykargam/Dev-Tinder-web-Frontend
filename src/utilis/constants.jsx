f81835f41c54b8e809cb2d0c61a1cad6c613ee8f
pranaykargam <sunnykargam@gmail.com>
2025-09-21 23:16:09 +0530

    updated

--- src/utilis/constants.jsx
+++ src/utilis/constants.jsx
@@ -1,8 +1,9 @@
-
-export const BASE_URL = "/api";
-
+// production
 // export const BASE_URL = "/api";
 
+// dev 
 
-
+export const BASE_URL = 
+location .hostname === "localhost"
+? "http://localhost:3000": "/api";
 
