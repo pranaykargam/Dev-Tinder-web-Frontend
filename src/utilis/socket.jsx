b9f78d9f4cb59de9878f6634a2e9e0447aea9af8
pranaykargam <sunnykargam@gmail.com>
2025-10-31 23:17:00 +0530

    updated

--- src/utilis/socket.jsx
+++ src/utilis/socket.jsx
@@ -11,14 +11,7 @@ export const createSocketConnection = () => {
 }
 
 
-// export const createSocketConnection = () => {
-//     if(location.hostname === "localhost") {
-    
-//     return io(BASE_URL);
-//     } else {
-//         return io("/" ,{path: "/api/socket.io"} );
-//     }
-// }
+
 
 
 
