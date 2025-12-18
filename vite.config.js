a9459e8332e89d4a0246ccca1a283776771810e7
pranaykargam <sunnykargam@gmail.com>
2025-12-18 22:37:32 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,4 @@ export default defineConfig({
 })
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
